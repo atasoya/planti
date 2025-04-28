7e0a50a0bc6919bb8bbffa98617dff9b74a7c282
atasoya <ataatasoy2013@gmail.com>
2025-04-28 19:17:36 +0300

    feature/add-middleware-frontend

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+const protectedRoutes = [
+  '/dashboard',
+];
+
+export async function middleware(request: NextRequest) {
+  console.log("Middleware running");
+  const { pathname } = request.nextUrl;
+  const session = request.cookies.get('plant-auth-token');
+  
+  if (pathname === '/login' && session) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+
+  const isProtectedRoute = protectedRoutes.some(route => 
+    pathname === route || pathname.startsWith(`${route}/`)
+  );
+  
+  if (isProtectedRoute && !session) {
+    const redirectUrl = new URL('/login', request.url);
+    
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return NextResponse.next();
+}
+
