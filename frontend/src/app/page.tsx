df4a2b71f7265ce15e3a2d9e9d3ea1c1f1684d86
atasoya <ataatasoy2013@gmail.com>
2025-04-29 13:51:52 +0300

    fix/add-main-links-to-index-page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div>
-      <h1>Planti</h1>
+      <a href="http://localhost:8025">Mailhog server</a>
+      <a href="http://localhost:3001/api/health">Planti API</a>
+      <a href="http://localhost:3000/dashboard">Planti Dashboard</a>
+      <a href="https://local.drizzle.studio/">Drizzle Studio</a>
     </div>
   );
 }
